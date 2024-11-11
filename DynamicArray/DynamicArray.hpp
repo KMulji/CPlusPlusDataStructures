a14384d36e0f14ca5e87f3c3bae0e30862fdeac8
KMulji <muljikyan@gmail.com>
2024-11-11 15:22:31 +0300

    skeleton append and pop
    need to add edge cases for append and pop. array can resize.

--- DynamicArray/DynamicArray.hpp
+++ DynamicArray/DynamicArray.hpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+
+
 template <class T>
 class DynamicArray
 {
@@ -8,27 +10,96 @@ private:
     int size;
     int length;
 
+    void Expand()
+    {
+        if(this->length==this->size)
+        {
+            T* temp = new T[size*2];
+            size=size*2;
+
+            for(int i=0;i<length;i++)
+            {
+                temp[i] = arr[i];
+            }
+        
+            arr=temp;
+        }
+    }
+    void Shrink()
+    {
+        int newSize= 0.5 *this->size;
+        
+        if(length == newSize)
+        {
+            std::cout<<"Resizing "<<std::endl;
+            T* temp = new T[newSize];
+            size=newSize;
+
+            for(int i=0;i<length;i++)
+            {
+                temp[i] = arr[i];
+            }
+             arr=temp;
+        }
+       
+    }
+
 public:
     DynamicArray()
     {
         arr = new T[10];
         this->size = 10;
-        this->length = 0;
+        this->length = -1;
+    }
+    int GetLength()
+    {
+        return length+1;
+    }
+    int GetSize()
+    {
+        return size;
     }
     DynamicArray(int A[], int n)
     {
         arr = new T[n];
         this->size = n;
-        this->length = 0;
+        this->length=-1;
         for (int i = 0; i < n; i++)
         {
-            arr[i] = A[i];
+            
             this->length++;
+            arr[length] = A[i];
+            
         }
     }
+    void Append(T val)
+    {
+        this->length++;
+        if(size == length)
+        {
+            Expand();
+        }
+        arr[length] = val;
+    }
+
+    int PopBack()
+    {
+        int ans = arr[length];
+        
+        
+       
+        if(this->length==this->size *0.5)
+        {
+            
+            Shrink();
+        }
+        this->length--;
+        return ans;
+    }
+
     void Display()
     {
-        for(int i=0;i<this->length;i++)
+        for(int i=0;i<=this->length;i++)
         {
             std::cout<<arr[i]<<std::endl;
         }
@@ -38,4 +109,6 @@ public:
     {
         delete[] arr;
     }
+
+    
 };
--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -2,10 +2,31 @@
 #include "DynamicArray.hpp"
 int main()
 {
-    std::cout << "Hello World" << std::endl;
-    int A[5] = {2,4,6,8,10};  
-
-    DynamicArray<int> da = DynamicArray<int>(A,5);
+    int A[6] = {2,4,6,8,10,12};  
+    DynamicArray<int> da = DynamicArray<int>(A,6);
+    da.Append(14);
     da.Display();
+    std::cout<<"Length is "<<da.GetLength()<<std::endl;
+    std::cout<<"Size is "<<da.GetSize()<<std::endl;
+
+    da.PopBack();
+    std::cout<<"Length is "<<da.GetLength()<<std::endl;
+    std::cout<<"Size is "<<da.GetSize()<<std::endl;
+
+    da.PopBack();
+    std::cout<<"Length is "<<da.GetLength()<<std::endl;
+    std::cout<<"Size is "<<da.GetSize()<<std::endl;
+
+    da.PopBack();
+    std::cout<<"Length is "<<da.GetLength()<<std::endl;
+    std::cout<<"Size is "<<da.GetSize()<<std::endl;
+
+    da.PopBack();
+    std::cout<<"Length is "<<da.GetLength()<<std::endl;
+    std::cout<<"Size is "<<da.GetSize()<<std::endl;
+
+    da.Display();
+    
+    
     return 0;
 }
